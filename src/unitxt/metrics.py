0ab6b03ae69f49aa74ef899320d6650ff05a457b
lilacheden <lilach.edel@gmail.com>
2024-08-20 11:32:46 +0300

    add GenerativeBinaryJudgeBAM Signed-off-by: lilacheden <lilach.edel@gmail.com>

--- src/unitxt/metrics.py
+++ src/unitxt/metrics.py
@@ -31,6 +31,7 @@ from .deprecation_utils import deprecation
 from .error_utils import Documentation, UnitxtWarning
 from .inference import (
     HFPipelineBasedInferenceEngine,
+    IbmGenAiInferenceEngine,
     InferenceEngine,
     OpenAiInferenceEngine,
     WMLInferenceEngine,
@@ -4928,6 +4929,12 @@ class GenerativeBinaryJudgeWML(GenerativeBinaryJudge):
     generation_kwargs = {"max_new_tokens": 5}
     _requirements_list: list[str] = ["ibm_watsonx_ai"]
     inference_engine_class = WMLInferenceEngine
+
+
+class GenerativeBinaryJudgeBAM(GenerativeBinaryJudge):
+    generation_kwargs = {"max_new_tokens": 5}
+    _requirements_list: list[str] = ["ibm-generative-ai"]
+    inference_engine_class = IbmGenAiInferenceEngine
 
 
 class GenerativeBinaryJudgeOpenAi(GenerativeBinaryJudge):
